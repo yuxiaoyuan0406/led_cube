de31b7ba8f6937bf2fe42e6351367425ccf78750
yuxiuaoyuan0406 <yuxiaoyuan0406@hotmail.com>
2021-09-25 21:26:24 +0800

    blink changed

--- led_cube.cpp
+++ led_cube.cpp
@@ -45,7 +45,7 @@ int main()
         while (1)
         {
             gpio_put(25, 1);
-            sleep_ms(100);
+            sleep_ms(50);
             gpio_put(25, 0);
             sleep_ms(300);
         }
